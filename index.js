a30cd92502778945c092e31dba4ea3be9f376213
Nat Welch <nat@natwelch.com>
2018-03-13 03:43:26 +0000

    Better logging

--- index.js
+++ index.js
@@ -5,6 +5,22 @@
  * @param {!Function} The callback function.
  */
 exports.subscribe = function subscribe(event, callback) {
+  const domains = {
+    "badtaco": "badta.co",
+    "cacophony": "cacophony.natwelch.com",
+    "giftionary": "giftionary.city",
+    "hello": "hello.natwelch.com",
+    "inspiration": "inspiration.natwelch.com",
+    "life": "life.natwelch.com",
+    "melandnat": "melandnat.com",
+    "natwelch": "natwelch.com"
+    "quotes": "quotes.natwelch.com",
+    "resume": "resume.natwelch.com",
+    "walls": "walls.natwelch.com",
+    "writing-be": "writing-be.natwelch.com",
+    "writing-fe": "writing.natwelch.com",
+  }
+
   // The Cloud Pub/Sub Message object.
   const pubsubMessage = event.data;
 
@@ -12,10 +28,14 @@ exports.subscribe = function subscribe(event, callback) {
   // it worked.
   let input = Buffer.from(pubsubMessage.data, 'base64').toString();
   let obj = JSON.parse(input);
-  console.log(obj);
+  console.log(`Recieved: ${obj}`);
 
   if (obj.status == "SUCCESS") {
-    console.log(obj.images);
+    obj.images.forEach(function(img) {
+      let url = img.split(":")[0];
+      let repo = url.split("/")[2];
+      console.log(`Update: kubectl patch deployment --namespace=${repo} ${domains[repo]} -p '{"spec":{"template":{"spec":{"containers":[{"name":"${repo}","image":"${img}"}]}}}}'`);
+    });
   }
 
   // Don't forget to call the callback.
